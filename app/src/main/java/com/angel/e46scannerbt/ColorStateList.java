052235469baa158744f386ea295efb2091aa9b1a
angelguerreromurga-collab <angelguerreromurga@gmail.com>
2026-05-16 03:02:32 +0200

    Add ColorStateList bridge for premium UI build

---
+++ app/src/main/java/com/angel/e46scannerbt/ColorStateList.java
@@ -0,0 +1,7 @@
+package com.angel.e46scannerbt;
+
+public class ColorStateList extends android.content.res.ColorStateList {
+    public ColorStateList(int[][] states, int[] colors) {
+        super(states, colors);
+    }
+}
